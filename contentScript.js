8c5cac52ec4b1891295482145e645709545e672b
rkostenk <romankoste@gmail.com>
2018-06-19 09:38:55 +0200

    Add social data to all Trade buttons. Add social trend widget

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,33 @@
+console.log("INIT");
+
+// Init Sentiments for every Trade button on the eTrading
+$("body").bind("DOMNodeInserted", function () {
+    $("button.trade-it:not(.updated)").each(function () {
+
+        // If we have already set - return
+        if ($(this).hasClass("updated")) {
+            return;
+        }
+
+        console.log("Init");
+
+        $(this).addClass("updated");
+        // $(this).css("background-color", "blue");
+
+        var percentage = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+        var color = (percentage < 50) ? "red" : "dodgerblue";
+
+        percentage = percentage < 50 ? percentage + 50 : percentage;
+
+        // TODO do it better with small chart or image ?
+        $(this).before("<span style='color: " + color + "'>" + percentage + "% </span>");
+    });
+});
 
 
-document.getElementById("viewContainer").style.cssText = "background-color: blue";
+// Init new widget on the screen
+$('.rightColumn:not(.updated)').prepend("<div id='social-trend-container'></div>");
+$('.rightColumn').addClass("updated");
 
-console.log(jQuery);
 
-$("button.trade-it").css("background-color", "blue");
-// console.log($);
-// document.addEventListener("DOMNodeInserted", function (event) {
-//   document.getElementsByClassName("AccountCard").
-// }, false);
+$('#social-trend-container').load(chrome.extension.getURL('socialTrend.html'));
