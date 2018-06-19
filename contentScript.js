ff59d178f661be184f7b35e6c0d3ab457bd9d8e8
Roman Kostenko <romankoste@gmail.com>
2018-06-19 06:58:38 +0200

    The first commit

---
+++ contentScript.js
@@ -0,0 +1,5 @@
+document.getElementById("viewContainer").style.cssText = "background-color: yellow";
+
+document.addEventListener("DOMNodeInserted", function (event) {
+  document.getElementsByClassName("AccountCard").
+}, false);
