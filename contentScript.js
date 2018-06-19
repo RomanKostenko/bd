039f44fed07358965c6d527ca321f2d7ebb124b2
Roman Kostenko <romankoste@gmail.com>
2018-06-19 07:47:31 +0200

    The second commit

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,11 @@
-document.getElementById("viewContainer").style.cssText = "background-color: yellow";
 
-document.addEventListener("DOMNodeInserted", function (event) {
-  document.getElementsByClassName("AccountCard").
-}, false);
+
+document.getElementById("viewContainer").style.cssText = "background-color: blue";
+
+console.log(jQuery);
+
+$("button.trade-it").css("background-color", "blue");
+// console.log($);
+// document.addEventListener("DOMNodeInserted", function (event) {
+//   document.getElementsByClassName("AccountCard").
+// }, false);
